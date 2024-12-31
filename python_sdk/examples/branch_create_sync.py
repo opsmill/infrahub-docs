0fbbfa4133fef6e2b23bbf0ce225116b86e3df9b
Pete Crocker <pete@petecrocker.com>
2024-12-31 13:11:50 +0000

    linting

--- python_sdk/examples/branch_create_sync.py
+++ python_sdk/examples/branch_create_sync.py
@@ -3,7 +3,9 @@ from infrahub_sdk import InfrahubClientSync
 
 def main():
     client = InfrahubClientSync(address="http://localhost:8000")
-    client.branch.create(branch_name="new-branch2", description="description", sync_with_git=False)
+    client.branch.create(
+        branch_name="new-branch2", description="description", sync_with_git=False
+    )
     print("New branch created")
 
 
