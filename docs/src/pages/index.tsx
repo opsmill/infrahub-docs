c9a6ed6298cf2a1b4047bdfdceeadea23b7c1ecc
Pete Crocker <pete@petecrocker.com>
2025-01-04 01:31:43 +0000

    added landing page

---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading'
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import Translate, { translate } from '@docusaurus/Translate';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">
+          {translate({
+            id: 'home.header.subtitle',
+            message: 'Simplify Infrastructure Automation',
+          })}
+        </p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/infrahub/"
+          >
+            {translate({
+              id: 'home.header.docs',
+              message: 'Infrahub Docs',
+            })}
+          </Link>
+          <Link
+            className="button button--info button--lg"
+            to="https://opsmill.instruqt.com/pages/labs"
+          >
+            {translate({
+              id: 'home.header.tutorials',
+              message: 'Tutorials',
+            })}
+          </Link>
+          <Link
+            className="button button--info button--lg"
+            to="https://demo.infrahub.app/"
+          >
+            {translate({
+              id: 'home.header.sandbox',
+              message: 'Sandbox',
+            })}
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+type SectionItem = {
+  title: string;
+  Svg: string;
+  description: React.JSX.Element;
+};
+
+// FIXME The SVG here (or all static files) cannot be loaded in i18n dev mode
+const SectionList: SectionItem[] = [
+  {
+    title: translate({
+      id: 'home.sectionList.development.title',
+      message: 'Development',
+    }),
+    Svg: '/img/infrahub.svg',
+    description: (
+      <Translate id="home.sectionList.development.description">
+        {
+          "These development packages such as the Infrahub Python SDK greatly simplify how you can interact with Infrahub programmatically."
+        }
+      </Translate>
+    ),
+  },
+  {
+    title: translate({
+      id: 'home.sectionList.integrations.title',
+      message: 'Integrations',
+    }),
+    Svg: '/img/infrahub.svg',
+    description: (
+      <Translate id="home.sectionList.integrations.description">
+        {'OpsMill maintains multiple integrations with other infrastructure systems. In addition, other companies maintain integrations with Infrahub.'}
+      </Translate>
+    ),
+  },
+
+  {
+    title: translate({
+      id: 'home.sectionList.demos.title',
+      message: 'Demos',
+    }),
+    Svg: '/img/infrahub.svg',
+    description: (
+      <Translate id="home.sectionList.demos.description">
+        {'These repositories demo key Infrahub features using example infrastructure. They demonstrate the capabilities to use Infrahub in various scenarios.'}
+      </Translate>
+    ),
+  },
+];
+
+function Section({ title, Svg, description }: SectionItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <img className={styles.sectionSvg} src={Svg} alt="" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as='h3'>{title}</Heading>
+        {/* <h3>{title}</h3> */}
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function HomepageSections(): React.JSX.Element {
+  return (
+    <section className={styles.sections}>
+      <div className="container">
+        <div className="row">
+          {SectionList.map((props, idx) => (
+            <Section key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default function Home(): React.JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Simplify Infrastructure Automation"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageSections />
+      </main>
+    </Layout>
+  );
+}
