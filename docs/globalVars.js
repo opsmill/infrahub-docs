062116f7fa76bec4983430adb621550ec75e6ee8
jeremy brisson <jeremy@opsmill.com>
2025-02-25 14:04:04 +0100

    config and clean for sdk

--- docs/globalVars.js
+++ docs/globalVars.js
@@ -1,5 +1,4 @@
-export default {
-    local_base_url_1: '../',
-    local_base_url_2: '../../',
-    local_base_url_branch : '../../../python_sdk/',
+
+  export default {
+    base_url: 'RELATIVE',
   };
