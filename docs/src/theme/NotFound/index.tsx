25174b2a3beb751f092887042a2200a27ae88dce
Fatih Acar <fatih@opsmill.com>
2025-09-04 14:47:59 +0200

    fix: restore 404 background and style
    Signed-off-by: Fatih Acar <fatih@opsmill.com>

--- docs/src/theme/NotFound/index.tsx
+++ docs/src/theme/NotFound/index.tsx
@@ -1,21 +1,20 @@
 import React, {type ReactNode} from 'react';
-import NotFound from '@theme-original/NotFound';
-import type NotFoundType from '@theme/NotFound';
-import type {WrapperProps} from '@docusaurus/types';
-import { useEffect } from 'react';
+import {translate} from '@docusaurus/Translate';
+import {PageMetadata} from '@docusaurus/theme-common';
+import Layout from '@theme/Layout';
+import NotFoundContent from '@theme/NotFound/Content';
 
-type Props = WrapperProps<typeof NotFoundType>;
-
-export default function NotFoundWrapper(props: Props): ReactNode {
-  useEffect(() => {
-    // Only run on the client (browser)
-    if (typeof window !== 'undefined' && typeof plausible === 'function') {
-      plausible('404');
-    }
-  }, []);
+export default function Index(): ReactNode {
+  const title = translate({
+    id: 'theme.NotFound.title',
+    message: 'Page Not Found',
+  });
   return (
     <>
-      <NotFound {...props} />
+      <PageMetadata title={title} />
+      <Layout>
+        <NotFoundContent />
+      </Layout>
     </>
   );
 }
