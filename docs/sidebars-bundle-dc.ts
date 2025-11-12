fa78828c5ab50b9efa476e0ed27a1831d5db1993
Pete Crocker <pete@petecrocker.com>
2025-11-12 09:36:50 +0000

    add bundle-dc docs placeholders

---
+++ docs/sidebars-bundle-dc.ts
@@ -0,0 +1,15 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  BundleDcSidebar: [
+    {
+      type: 'category',
+      label: 'Getting started',
+      items: [
+        'readme',
+      ],
+    },
+  ]
+};
+
+export default sidebars;
