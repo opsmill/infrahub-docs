c8b20e99eb8de50d4f78897bed4b0c8007720e09
Wim Van Deun <wimvandeun@gmail.com>
2026-04-01 09:56:52 +0000

    add infrahub-solution-ai-dc (#148)
    * add infrahub-solution-ai-dc
    
    * add flake.nix
    
    * fix
    
    * typo
    
    * typo
    
    ---------
    
    Co-authored-by: Wim Van Deun <wim@vdwitconsulting.be>
    Co-authored-by: Pete Crocker <pete@petecrocker.com>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Infrahub Docs development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            stdenv.cc.cc.lib
+            nodejs_24
+            markdownlint-cli2
+            yamllint
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+          ];
+        };
+      }
+    );
+}
